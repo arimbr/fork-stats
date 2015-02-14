bae880e4595165b6d8b2ade7c9343dea74217db9
arimbr <arimbr@gmail.com>
2015-02-14 13:24:16 +0200

    Implement ForkController

--- app.js
+++ app.js
@@ -3,4 +3,19 @@
 *
 * Get forks data for a Github repository
 */
-var app = angular.module('forks', []);
+(function() {
+// Self executing function to ensure that code is not declared
+// in the global scope
+    var app = angular.module('forks', []);
+
+    // Implement Fork controller
+    app.controller('ForkController', function(){
+        this.search = {};  // initialize search data
+
+        this.getForks = function() {
+            console.log("User: " + this.search.user);  // log input search data
+            console.log("Repository: " + this.search.repo);
+            this.search = {};  // clear form data after submit
+        };
+    });
+})();
