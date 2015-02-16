6c94397516e399c798a0a604757489d05c3fe917
Bao Nguyen <baonguyen1201@gmail.com>
2015-02-16 20:53:37 +0200

    Add button for how many forks to show per page

--- js/app.js
+++ js/app.js
@@ -6,12 +6,12 @@
 (function() {
 // Self executing function to ensure that code is not declared
 // in the global scope
-    var app = angular.module('forks', ['ngResource']);
+    var app = angular.module('forks', ['ngResource','ui.bootstrap']);
 
     // Add factory Forks
     // http://www.sitepoint.com/creating-crud-app-minutes-angulars-resource/
     app.factory('Forks', ['$resource', function($resource){
-        return $resource('https://api.github.com/repos/:user/:repo/forks');
+        return $resource('https://api.github.com/repos/:user/:repo/forks?per_page=:perPage');
     }]);
 
     // Implement Fork controller
@@ -20,9 +20,13 @@
         $scope.error = false;
         $scope.user = "angular";
         $scope.repo = "angular";
+        $scope.perPage = '30'; // Set default choice to 30 (options: 30, 50, 100)
 
         $scope.getForks = function() {
-            Forks.query({user: $scope.user, repo: $scope.repo},
+
+            // DONE: Show a chosen number of forks on a repo
+            // TODO: Pagination
+            Forks.query({user: $scope.user, repo: $scope.repo, perPage : $scope.perPage},
                 function(data) {  // query expects array data
                     $scope.forks = data;
                     $scope.error = false;
@@ -35,7 +39,6 @@
                 }
             );
         };
-
         $scope.getForks();  // Get forks for Angular
     }]);
 })();
