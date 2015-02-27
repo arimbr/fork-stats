8056cde741d7fb0d353d099acc2fa13685b17c30
arimbr <arimbr@gmail.com>
2015-02-27 11:14:34 +0200

    Code revision

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 /**
 * forks Module
 *
-* Get forks data for a Github repository
+* Gets forks data for a Github repository
 */
 (function() {
 // Self executing function to ensure that code is not declared
 // in the global scope
-    var app = angular.module('forks', ['ui.bootstrap', 'forksControllers']);
+    angular.module('forks', ['ui.bootstrap', 'forksControllers']);
 })();
